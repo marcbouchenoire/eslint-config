af630ac2427e4d4793a3cb8e0b4132df2a1d7645
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-07-15 12:28:34 +0200

    Add React rules

--- react.js
+++ react.js
@@ -1,6 +1,14 @@
 module.exports = {
   extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
   plugins: ["react", "react-hooks"],
+  rules: {
+    "react/display-name": "off",
+    "react/jsx-no-useless-fragment": "warn",
+    "react/jsx-pascal-case": "error",
+    "react/jsx-sort-props": "warn",
+    "react/prop-types": "off",
+    "react/style-prop-object": "error"
+  },
   settings: {
     react: {
       version: "detect"
