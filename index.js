450b642e7a39f54e078fdaf8bb721a466ca4a8dd
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-07-20 11:04:02 +0200

    Add parser options

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ module.exports = {
     "prettier"
   ],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json"
+  },
   plugins: [
     "@typescript-eslint",
     "typescript-sort-keys",
