e8c52b4756b2820b3c7c08bf5a8ad2561ae58ce6
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-07-26 09:38:40 +0200

    Fix default unresolved imports

--- index.js
+++ index.js
@@ -61,6 +61,9 @@ module.exports = {
   },
   settings: {
     "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ",tsx"]
+      },
       typescript: {
         project: "tsconfig.json"
       }
