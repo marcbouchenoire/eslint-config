e5f0944081c760e1d961b089ffbccf76bfdd18b6
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2021-12-21 21:01:05 +0100

    Add JSDoc configuration

---
+++ jsdoc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  plugins: ["jsdoc"],
+  rules: {
+    "jsdoc/check-alignment": "warn",
+    "jsdoc/check-indentation": "warn",
+    "jsdoc/check-param-names": "warn",
+    "jsdoc/check-property-names": "warn",
+    "jsdoc/check-tag-names": "warn",
+    "jsdoc/check-values": "warn",
+    "jsdoc/empty-tags": "warn",
+    "jsdoc/multiline-blocks": "warn",
+    "jsdoc/newline-after-description": "warn",
+    "jsdoc/no-multi-asterisks": "warn",
+    "jsdoc/no-types": "warn",
+    "jsdoc/require-description": "warn",
+    "jsdoc/require-description-complete-sentence": "warn",
+    "jsdoc/require-hyphen-before-param-description": "warn",
+    "jsdoc/require-jsdoc": [
+      "warn",
+
+      {
+        contexts: [
+          "ExportNamedDeclaration[declaration.type='TSDeclareFunction']:not(ExportNamedDeclaration[declaration.type='TSDeclareFunction'] + ExportNamedDeclaration[declaration.type='TSDeclareFunction'])",
+          "ExportNamedDeclaration[declaration.type='FunctionDeclaration']:not(ExportNamedDeclaration[declaration.type='TSDeclareFunction'] + ExportNamedDeclaration[declaration.type='FunctionDeclaration'])"
+        ],
+        require: {
+          FunctionDeclaration: false
+        }
+      }
+    ],
+    "jsdoc/require-param": "warn",
+    "jsdoc/require-param-description": "warn",
+    "jsdoc/require-param-name": "warn",
+    "jsdoc/require-property": "warn",
+    "jsdoc/require-property-description": "warn",
+    "jsdoc/require-property-name": "warn"
+  }
+}
